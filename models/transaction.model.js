6528e526a15139235183b2bae62d6ceda3e322ca
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2024-02-15 01:44:35 +0530

    error fixed

--- models/transaction.model.js
+++ models/transaction.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const transactionSchema =new mongoose.Schema({
     description:{type:String,required:true},
     type:{type:String,required:true},
-    amount:{type:String,required:true}
+    amount:{type:number,required:true}
 },
 {timestamps:true});
 
