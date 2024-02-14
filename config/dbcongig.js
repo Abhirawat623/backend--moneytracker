7091e6b5ae6024f24a7b193cba49e2efb4d4b5ac
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2024-02-14 17:15:16 +0530

    mongodb added

--- config/dbcongig.js
+++ config/dbcongig.js
@@ -1,10 +1,12 @@
-const mongooose = require('mongooose');
+const mongooose = require('mongoose');
 
 const connectDb= async()=>{
     try{
-
+    await mongooose.connect(process.env.DATABASE_URI)
     }
     catch(err){
         console.log(err)
     }
 }
+
+module.exports=connectDb;
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const connectDb =require("./config/dbcongig");
 //config
 dotenv.config();
+//mongodb conf
+connectDb();
 //app
 const app =express();
 app.use(cors());
@@ -15,6 +18,14 @@ app.get("/",(req,res)=>{
     res.send("hello user")
 });
 
-app.listen(PORT,()=>{
-    console.log("server running")
+// app.listen(PORT,()=>{
+//     console.log("server running")
+// })
+
+//mongo connect
+mongoose.connection.once("open",()=>{
+    console.log("mongo connected");
+    app.listen(process.env.PORT || PORT,()=>{
+        console.log("mongo and server running")
+    })
 })
