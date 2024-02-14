ef794828655f2517f645ab0f40631608fd55a029
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2024-02-15 01:51:05 +0530

    deploying

--- models/transaction.model.js
+++ models/transaction.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const transactionSchema =new mongoose.Schema({
     description:{type:String,required:true},
     type:{type:String,required:true},
-    amount:{type:number,required:true}
+    amount:{type:Number,required:true}
 },
 {timestamps:true});
 
